83d876f8ee24f7a57d70acbc0706bdefea02a9e8
MoFunCode <mohamed.sul02@gmail.com>
2025-05-28 09:37:58 -0500

    feat: PriceCalculator with sandwich pricing logic

--- DELI-cious/src/main/java/Main.java
+++ DELI-cious/src/main/java/Main.java
@@ -3,21 +3,14 @@ import com.delicious.model.Topping;
 import com.delicious.model.enums.BreadType;
 import com.delicious.model.enums.SandwichSize;
 import com.delicious.model.enums.ToppingType;
+import com.delicious.service.PriceCalculator;
 
 public class Main {
     public static void main(String[] args) {
 
-        Sandwich sandwich = new Sandwich(
-                SandwichSize.EIGHT_INCH,
-                BreadType.WHITE,
-                true
-        );
-
-        sandwich.addTopping(new Topping(ToppingType.BACON, true));
-        sandwich.addTopping(new Topping(ToppingType.AMERICAN_CHEESE, false));
-        sandwich.addTopping(new Topping(ToppingType.LETTUCE, false));
-
-        System.out.println(sandwich);
+        Sandwich sandwich = new Sandwich(SandwichSize.EIGHT_INCH, BreadType.WHITE, false);
+        sandwich.addTopping(new Topping(ToppingType.BACON, false));
+        System.out.println("Price: $" + PriceCalculator.calculateSandwichPrice(sandwich));
     }
 
 }
--- DELI-cious/src/main/java/com/delicious/service/PriceCalculator.java
+++ DELI-cious/src/main/java/com/delicious/service/PriceCalculator.java
@@ -1,4 +1,21 @@
 package com.delicious.service;
 
+import com.delicious.model.Sandwich;
+import com.delicious.model.Topping;
+import com.delicious.model.enums.SandwichSize;
+
 public class PriceCalculator {
+    public static double calculateSandwichPrice(Sandwich sandwich) {
+        double price = sandwich.getSize().getBasePrice();
+
+        for (Topping topping : sandwich.getToppings()) {
+            if (topping.getType().isPremium()) {
+                price += topping.isExtra() ?
+                        sandwich.getSize().getBasePrice() * 0.1 :
+                        sandwich.getSize().getBasePrice() * 0.2;
+            }
+        }
+        return price;
+    }
 }
+
