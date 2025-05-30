2176a0ea9e5dd5d326ae30cb43ec370d44ed0da5
MoFunCode <mohamed.sul02@gmail.com>
2025-05-29 20:58:37 -0500

    Simplified ToppingType Enum

--- DELI-cious/src/main/java/com/delicious/model/enums/ToppingType.java
+++ DELI-cious/src/main/java/com/delicious/model/enums/ToppingType.java
@@ -2,46 +2,46 @@ package com.delicious.model.enums;
 
 public enum ToppingType {
 
-    STEAK("Steak", true, ToppingCategory.MEAT),
-    HAM("Ham", true, ToppingCategory.MEAT),
-    SALAMI("Salami", true, ToppingCategory.MEAT),
-    ROAST_BEEF("Roast Beef", true, ToppingCategory.MEAT),
-    CHICKEN("Chicken", true, ToppingCategory.MEAT),
-    BACON("Bacon", true, ToppingCategory.MEAT),
 
-    AMERICAN_CHEESE("American", true, ToppingCategory.CHEESE),
-    PROVOLONE("Provolone", true, ToppingCategory.CHEESE),
-    CHEDDAR("Cheddar", true, ToppingCategory.CHEESE),
-    SWISS("Swiss", true, ToppingCategory.CHEESE),
+    STEAK("Steak", true),
+    HAM("Ham", true),
+    SALAMI("Salami", true),
+    ROAST_BEEF("Roast Beef", true),
+    CHICKEN("Chicken", true),
+    BACON("Bacon", true),
 
-    LETTUCE("Lettuce", false, ToppingCategory.REGULAR),
-    PEPPERS("Peppers", false, ToppingCategory.REGULAR),
-    ONIONS("Onions", false, ToppingCategory.REGULAR),
-    TOMATOES("Tomatoes", false, ToppingCategory.REGULAR),
-    JALAPEÑOS("Jalapeños", false, ToppingCategory.REGULAR),
-    CUCUMBERS("Cucumbers", false, ToppingCategory.REGULAR),
-    PICKLES("Pickles", false, ToppingCategory.REGULAR),
-    GUACAMOLE("Guacamole", false, ToppingCategory.REGULAR),
-    MUSHROOMS("Mushrooms", false, ToppingCategory.REGULAR),
+    AMERICAN("American", true),
+    PROVOLONE("Provolone", true),
+    CHEDDAR("Cheddar", true),
+    SWISS("Swiss", true),
 
-    MAYO("Mayo", false, ToppingCategory.SAUCE),
-    MUSTARD("Mustard", false, ToppingCategory.SAUCE),
-    KETCHUP("Ketchup", false, ToppingCategory.SAUCE),
-    RANCH("Ranch", false, ToppingCategory.SAUCE),
-    THOUSAND_ISLANDS("Thousand Islands", false, ToppingCategory.SAUCE),
-    VINAIGRETTE("Vinaigrette", false, ToppingCategory.SAUCE),
+    LETTUCE("Lettuce", false),
+    PEPPERS("Peppers", false),
+    ONIONS("Onions", false),
+    TOMATOES("Tomatoes", false),
+    JALAPEÑOS("Jalapeños", false),
+    CUCUMBERS("Cucumbers", false),
+    PICKLES("Pickles", false),
+    GUACAMOLE("Guacamole", false),
+    MUSHROOMS("Mushrooms", false),
 
-    AU_JUS("Au Jus", false, ToppingCategory.SIDE),
-    SAUCE("Sauce", false, ToppingCategory.SIDE);
+    MAYO("Mayo", false),
+    MUSTARD("Mustard", false),
+    KETCHUP("Ketchup", false),
+    RANCH("Ranch", false),
+    THOUSAND_ISLANDS("Thousand Islands", false),
+    VINAIGRETTE("Vinaigrette", false),
+
+    AU_JUS("Au Jus", false),
+    SAUCE("Sauce", false);
 
     private final String displayName;
     private final boolean isPremium;
-    private final ToppingCategory category;
 
-    ToppingType(String displayName, boolean isPremium, ToppingCategory category) {
+
+    ToppingType(String displayName, boolean isPremium) {
         this.displayName = displayName;
         this.isPremium = isPremium;
-        this.category = category;
     }
 
     public String getDisplayName() {
@@ -52,11 +52,6 @@ public enum ToppingType {
         return isPremium;
     }
 
-    public ToppingCategory getCategory() {
-        return category;
-    }
 
-    public enum ToppingCategory {
-        MEAT, CHEESE, REGULAR, SAUCE, SIDE
-    }
 }
+
