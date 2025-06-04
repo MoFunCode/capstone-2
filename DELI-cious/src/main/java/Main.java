a246659a6c29c92d1f44886f9c7f5c6a3a3e77f8
MoFunCode <mohamed.sul02@gmail.com>
2025-06-04 08:31:39 -0500

    add SandwichSize Enum class

--- DELI-cious/src/main/java/Main.java
+++ DELI-cious/src/main/java/Main.java
@@ -1,10 +1,11 @@
 import com.delicious.cli.DeliApp;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        DeliApp deliApp = new DeliApp();
-        deliApp.start();
+
     }
+
 }
 
---
+++ DELI-cious/src/main/java/com/delicious/model/enums/SandwichSize.java
@@ -0,0 +1,25 @@
+package com.delicious.model.enums;
+
+public enum SandwichSize {
+
+    FOUR_INCH("4\"", 5.50),
+    EIGHT_INCH("8\"", 7.00),
+    TWELVE_INCH("12\"", 8.50);
+
+    private final String displayName;
+    private final double basePrice;
+
+    SandwichSize(String displayName, double basePrice) {
+        this.displayName = displayName;
+        this.basePrice = basePrice;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+}
